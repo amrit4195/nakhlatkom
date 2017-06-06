8c24d58f36f6e4cebb1992eb756f317d482c8ca0
amrit <amrit.pal445@gmail.com>
2017-06-06 15:12:25 +1000

    -

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     	<meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Test </title>
+        <title>Testing 123</title>
         <!-- Vendor CSS -->
         <link href="/dp/vendors/animate.css/animate.min.css" rel="stylesheet">
         <link href="/dp/vendors/material-design-iconic-font/dist/css/material-design-iconic-font.min.css" rel="stylesheet">
