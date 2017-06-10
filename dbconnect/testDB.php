4473cc783dc3795c4f4d264b74c0a8d99b2bb1b0
Amrit <amrit.pal445@gmail.com>
2017-06-10 19:43:48 +1000

    -

--- dbconnect/testDB.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_SERVER', 'sydneynakhlatkom.ccx7gtfezsye.ap-southeast-2.rds.amazonaws.com');
-define('DB_USERNAME', 'amrit4195');
-define('DB_PASSWORD', 'amrit4195');
-define('DB_DATABASE', 'nakhlatkom');
-
-?>
